7d50b173207092d7ccd58059151c4143f70078d0
Shady Ali <121682078+SHIXOOM@users.noreply.github.com>
2024-12-31 22:35:55 +0200

    LMEA Adaptive Temperature Implemented

--- src/Solvers/TinderMatchingSolver.py
+++ src/Solvers/TinderMatchingSolver.py
@@ -26,25 +26,28 @@ class TinderMatchingSolver(LLMTSPSolver):
         """
 
         MAX_GENERATIONS = 250
-        POPULATION_SIZE = 30
         NODE_COUNT = problem.dimension
 
+        populationSize = 30
         currentModelTemperature = 1
-        population = self.population_initializer.initialize(POPULATION_SIZE, problem)
+        population = self.population_initializer.initialize(populationSize, problem)
 
         """ Configure model """
-        systemPrompt = PRManager.getSystemPrompt(populationSize=POPULATION_SIZE)
+        systemPrompt = PRManager.getSystemPrompt(populationSize=populationSize)
         self.model.configure(systemPrompt, currentModelTemperature)
 
         pointsCoordinatesPairs = {i: (j[0], j[1]) for i, j in problem.node_coords.items()}
         bestSolutionLength = population[-1][1]
+        
+        worseIterations  = 0
         for generation in range(1, MAX_GENERATIONS + 1):
+            
             print(f"""
                 best sol: {bestSolutionLength}
                 Generation: {generation}
                 _________________________________________________________________________________
-                  """)
-            if population[-1][1] == 417:
+                """)
+            if population[-1][1] == problem_optimal_distance:
                 return population[-1][0], generation
 
             """
@@ -55,19 +58,40 @@ class TinderMatchingSolver(LLMTSPSolver):
             newGenPrompt = PRManager.getNewGenerationPrompt(population, pointsCoordinatesPairs, 30)
             newGenResponse = self.model.run(newGenPrompt)
             print(f"""___________________________________________________________________________
-                  {newGenResponse}
-                  """)
+                {newGenResponse}
+                """)
+            
             newGenerationTraces = PRManager.parseNewGeneration(newGenResponse, nodeCount=NODE_COUNT)
             # TODO: ADD logging
 
             # get the lengths of the new generation traces
+            newPopulation = []
             for trace in newGenerationTraces:
                 length = problem.trace_tours([trace])
-                population.append((trace, length[0]))
+                newPopulation.append((trace, length[0]))
             
+            # sort new population by length descendingly
+            newPopulation = sorted(newPopulation, key=lambda x: x[1], reverse=True)
+            
+            if len(newPopulation) > 0:
+                if newPopulation[-1][1] >= bestSolutionLength:
+                    worseIterations += 1
+                else:
+                    worseIterations = 0
+                    
+            if worseIterations > 20:
+                worseIterations = 0
+                if currentModelTemperature < 2:
+                    currentModelTemperature += 0.05
+                    populationSize += 2
+            
+            newPopulation = list(filter(lambda x: all(item[0]!=x[0] for item in population), newPopulation))
+            
+                
+            population.extend(newPopulation)
             # sort the population by the tour lengths descendingly
             population = sorted(population, key=lambda x: x[1], reverse=True)
-            population = population[-POPULATION_SIZE:]
+            population = population[-populationSize:]
             print(f"""
                   {population}
                   """)
