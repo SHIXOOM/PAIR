d1635033d986f690a6eb1a0fe2d11fe1a11b242d
Shady Ali <121682078+SHIXOOM@users.noreply.github.com>
2025-05-06 22:08:58 +0300

    Fix: migrated Gemini api from generativeai to genai as the earlier is deprecated. Added new gemini 2.5 flash model in options (Thinking configurations is not done yet so it might not work properly). Minor refactoring and cleaning.

--- __main__.py
+++ __main__.py
@@ -1,7 +1,9 @@
-from src.PopulationInitializers.RandomInitializer import RandomInitializer
-from src.PopulationInitializers.SAPopulationInitializer import SAPopulationInitializer as SAInitializer
 from src.ExperimentRunner import ExperimentRunner
 from src.Models.Gemini import Gemini
+from src.PopulationInitializers.RandomInitializer import RandomInitializer
+from src.PopulationInitializers.SAPopulationInitializer import (
+    SAPopulationInitializer as SAInitializer,
+)
 from src.Solvers.TinderMatchingSolver import TinderMatchingSolver
 
 
@@ -20,29 +22,37 @@ def select_option(options, prompt):
 def get_experiment_inputs():
     problem_name = get_user_input("Enter the name of the problem: ")
     tsp_path = get_user_input("Enter the path to the problem file: ")
-    optimal_distance = float(get_user_input("Enter the optimal solution for the problem: "))
+    optimal_distance = float(
+        get_user_input("Enter the optimal solution for the problem: ")
+    )
 
     solvers = ["TinderMatching"]
     solver_name = select_option(solvers, "Select the solver to use:")
 
-    models = ["gemini-2.0-flash-thinking-exp-1219", "gemini-2.0-flash-exp"]
+    models = [
+        "gemini-2.0-flash-thinking-exp-1219",
+        "gemini-2.0-flash-exp",
+        "gemini-2.5-flash-preview-04-17",
+    ]
     model_name = select_option(models, "Select the model to use:")
 
     population_initializers = ["simulated-annealing", "random"]
-    population_initializer_name = select_option(population_initializers, "Select the population initializer to use:")
+    population_initializer_name = select_option(
+        population_initializers, "Select the population initializer to use:"
+    )
 
     return {
-        'problem_name': problem_name,
-        'tsp_path': tsp_path,
-        'optimal_distance': optimal_distance,
-        'solver_name': solver_name,
-        'model_name': model_name,
-        'population_initializer_name': population_initializer_name
+        "problem_name": problem_name,
+        "tsp_path": tsp_path,
+        "optimal_distance": optimal_distance,
+        "solver_name": solver_name,
+        "model_name": model_name,
+        "population_initializer_name": population_initializer_name,
     }
 
 
 def initialize_experiment(inputs):
-    """ Sets Experiment up based on given inputs """
+    """Sets Experiment up based on given inputs"""
 
     """ Can be refactored using Factory pattern but 
         for simplicity, we settled for current approach,
@@ -50,33 +60,35 @@ def initialize_experiment(inputs):
         be refactored. """
 
     """ Initialize population initializer """
-    if inputs['population_initializer_name'] == "simulated-annealing":
+    if inputs["population_initializer_name"] == "simulated-annealing":
         population_initializer = SAInitializer()
-    elif inputs['population_initializer_name'] == "random":
+    elif inputs["population_initializer_name"] == "random":
         population_initializer = RandomInitializer()
     else:
         raise Exception("Population initializer not found.")
 
     """ Initialize model """
-    if inputs['model_name'] == "gemini-2.0-flash-thinking-exp-1219":
-        model = Gemini("system_prompt", 1, inputs['model_name'])
-    elif inputs['model_name'] == "gemini-2.0-flash-exp":
-        model = Gemini("system_prompt", 1, inputs['model_name'])
+    if inputs["model_name"] == "gemini-2.0-flash-thinking-exp-1219":
+        model = Gemini("system_prompt", 1, inputs["model_name"])
+    elif inputs["model_name"] == "gemini-2.0-flash-exp":
+        model = Gemini("system_prompt", 1, inputs["model_name"])
+    elif inputs["model_name"] == "gemini-2.5-flash-preview-04-17":
+        model = Gemini("system_prompt", 1, inputs["model_name"])
     else:
         raise Exception("Model not found")
 
     """ Initialize solver"""
-    if inputs['solver_name'] == "TinderMatching":
+    if inputs["solver_name"] == "TinderMatching":
         solver = TinderMatchingSolver(model, population_initializer)
     else:
         raise Exception("Solver not found")
 
     return ExperimentRunner(
-        inputs['problem_name'],
-        inputs['tsp_path'],
-        inputs['optimal_distance'],
+        inputs["problem_name"],
+        inputs["tsp_path"],
+        inputs["optimal_distance"],
         solver,
-        model
+        model,
     )
 
 
@@ -86,5 +98,5 @@ def main():
     experiment_runner.run()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
--- src/Models/Gemini.py
+++ src/Models/Gemini.py
@@ -1,25 +1,30 @@
-from src.Models.Model import Model
-import google.generativeai as genai
-
 import os
-from dotenv import load_dotenv
 import time
 
-from src.PromptResponseManager.PromptResponseManager import PromptResponseManager as PRManager
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
 
+from src.Models.Model import Model
+from src.PromptResponseManager.PromptResponseManager import (
+    PromptResponseManager as PRManager,
+)
 
 class Gemini(Model):
     """
-       Example concrete implementation of the Model class 
+    Example concrete implementation of the Model class
     """
 
-    def __init__(self, systemPrompt: str, temperature: float, modelName="gemini-2.0-flash-thinking-exp"):
+    def __init__(
+        self,
+        systemPrompt: str,
+        temperature: float,
+        modelName="gemini-2.0-flash-thinking-exp",
+    ):
         super().__init__(systemPrompt, temperature, modelName)
 
-        self.generation_config = None
         self.systemPrompt = None
         self.client = None
-        self.generationConfig = None
 
         # Load environment variables
         load_dotenv()
@@ -32,35 +37,31 @@ class Gemini(Model):
         self.temperature = temperature
         self.systemPrompt = systemPrompt
 
-        # Models parameters used in generation
-        self.generationConfig = {
-            "temperature": temperature,
-        }
-
         # Initialize Gemini Client
-        genai.configure(api_key=os.environ.get('GEMINI_API_KEY'))
-        self.client = genai.GenerativeModel(
-            model_name=self.modelName,
-            generation_config=self.generationConfig,
-            system_instruction=systemPrompt
-        )
+        self.client = genai.Client(api_key=os.environ.get("GOOGLE_API_KEY"))
 
     def run(self, prompt: str) -> str:
         errors = 0
         while True:
             try:
-                if self.modelName.__contains__("thinking"):
-                    response = self.client.generate_content(prompt).candidates[0].content.parts[1].text
-                else:
-                    response = self.client.generate_content(prompt).candidates[0].content.parts[0].text
-
-                return response
+                response = self.client.models.generate_content(
+                    model=self.modelName,
+                    contents=prompt,
+                    config=types.GenerateContentConfig(
+                        system_instruction=self.systemPrompt,
+                        temperature=self.temperature,
+                    ),
+                )
+                return response.text
             except Exception as e:
                 print(f"Error while making the Model's call: {e}")
-                if (errors+1) % 10 == 0:
-                    shouldContinue = \
-                        input(f"Api provider responded with {errors} errors in a row. do you want to continue? (Y/N)"
-                            ) == "Y"
+                if (errors + 1) % 10 == 0:
+                    shouldContinue = (
+                        input(
+                            f"Api provider responded with {errors} errors in a row. do you want to continue? (Y/N)"
+                        )
+                        == "Y"
+                    )
 
                     if not shouldContinue:
                         raise e
@@ -71,12 +72,7 @@ class Gemini(Model):
 
     def set_temperature(self, temperature: float):
         self.temperature = temperature
-        self.generation_config["temperature"] = temperature
 
-        self.client = genai.GenerativeModel(
-            model_name=self.modelName,
-            generation_config=self.generation_config,
-            system_instruction=self.systemPrompt
-        )
+        self.client = genai.Client(api_key=os.environ.get("GOOGLE_API_KEY"))
         print(f"Temperature set to {temperature}")
         # set llm temp
